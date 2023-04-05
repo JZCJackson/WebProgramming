4a0853018f9a34a1d0a0fca536f0dc11e2dd9dda
UncleGeorge <george.cosmoswill@gmail.com>
2023-04-05 17:54:51 -0400

    finish update record
    finish update record

--- routes/api/data.js
+++ routes/api/data.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Import Person schema
 const Shipwreck = require('./../../models/Shipwreck')
 
+// post a new record
 router.post('/', (req, res) => {
 
     const newShipwreck = Shipwreck({
@@ -29,13 +30,42 @@ router.post('/', (req, res) => {
 
 })
 
+// get by id
 router.get('/:id', (req, res) => {
     Shipwreck
-        .findOne({_id: req.params.id})
+        .findOne({ _id: req.params.id })
         .then(shipwreck => res.send(shipwreck))
-        .catch(err => console.log(err))
+        .catch(err => res.status(500).send(err))
 })
 
-
+// update record
+router.put('/:id', (req, res) => {
+    Shipwreck.updateOne(
+        { _id: req.params.id },
+        {
+            $set: {
+                recrd: req.body.recrd,
+                vesslterms: req.body.vesslterms,
+                feature_type: req.body.feature_type,
+                chart: req.body.chart,
+                latdec: req.body.latdec,
+                londec: req.body.londec,
+                gp_quality: req.body.gp_quality,
+                depth: req.body.depth,
+                sounding_type: req.body.sounding_type,
+                history: req.body.history,
+                quasou: req.body.quasou,
+                watlev: req.body.watlev,
+                coordinates: req.body.coordinates
+            }
+        })
+        .exec()
+        .then(() => {
+            res.status(201).send('Record Updated: ' + req.params.id)
+        })
+        .catch((err) => {
+            res.status(500).send(err)
+        })
+})
 
 module.exports = router
